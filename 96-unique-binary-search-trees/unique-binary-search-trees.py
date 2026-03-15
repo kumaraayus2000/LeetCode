24a9d11559d306bccb803fbfd9ef316869864cc1
kumaraayus2000 <kumar.aayus@northeastern.edu>
2026-03-15 18:30:59 -0400

    Time: 0 ms (100.00%) | Memory: 19.1 MB (96.57%) - LeetSync

---
+++ 96-unique-binary-search-trees/unique-binary-search-trees.py
@@ -0,0 +1,16 @@
+class Solution:
+    def numTrees(self, n: int) -> int:
+        dp = [0] * (n + 1)
+
+        dp[0] = 1
+        dp[1] = 1
+
+        for node in range(2, n + 1):
+            total = 0
+            for i in range(1, node + 1):
+                left = i - 1
+                right = node - i
+                total += dp[left] * dp[right]
+            dp[node] = total
+        
+        return dp[n]
