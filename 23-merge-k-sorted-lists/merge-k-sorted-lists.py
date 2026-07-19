935943fa01a595daa16e70ec296051e79ef5e1f7
kumaraayus2000 <kumar.aayus@northeastern.edu>
2026-07-18 20:02:10 -0400

    Time: 863 ms (12.74%) | Memory: 22.8 MB (69.04%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -6,21 +6,40 @@
 class Solution:
 
     def merge(self,list1:Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        # if list1 is None:
+        #     return list2
+
+        # if list2 is None:
+        #     return list1
+
+        # if list1.val < list2.val:
+        #     list1.next = self.merge(list1.next,list2)
+        #     return list1
+        # else:
+        #     list2.next = self.merge(list1,list2.next)
+        #     return list2
+        
+        dummy = ListNode()
+        tail = dummy
+
+        while list1 is not None and list2 is not None:
+            if list1.val < list2.val:
+                tail.next = list1
+                list1=list1.next
+            else:
+                tail.next = list2
+                list2=list2.next
+
+            tail=tail.next
+
+
         if list1 is None:
-            return list2
+            tail.next = list2
 
         if list2 is None:
-            return list1
-
-        if list1.val < list2.val:
-            list1.next = self.merge(list1.next,list2)
-            return list1
-        else:
-            list2.next = self.merge(list1,list2.next)
-            return list2
+            tail.next = list1
         
-        
-
+        return dummy.next
 
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
 
