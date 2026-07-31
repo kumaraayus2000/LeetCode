1f6dc588d76797dd09e4c9c11604da2c9cfcb2a5
kumaraayus2000 <kumar.aayus@northeastern.edu>
2026-07-30 23:38:37 -0400

    Time: 91 ms (47.56%) | Memory: 19.7 MB (15.03%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -3,22 +3,20 @@ class Solution:
         
         mp = {}
 
-        r = 0
-        maxfreq = 0
-        l = 0
+        r =0
+        l=0
         maxlen = 0
+        maxfreq = 0 
+
         while r < len(s):
-            mp[s[r]] = mp.get(s[r],0) +1
-            maxfreq = max(maxfreq,mp[s[r]])
+
+            mp[s[r]] = mp.get(s[r],0) + 1
+            maxfreq = max(maxfreq, mp[s[r]])
             while (r-l)+1 - maxfreq > k:
                 mp[s[l]]-=1
                 l+=1
 
-            maxlen = max(maxlen, (r-l)+1)
+            maxlen = max(maxlen,(r-l)+1)
             r+=1
 
         return maxlen
-
-
-
-        
