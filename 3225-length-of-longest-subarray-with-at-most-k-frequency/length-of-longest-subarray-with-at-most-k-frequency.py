aaadd8b87b6c00fda9399a16683c776c43875db3
kumaraayus2000 <kumar.aayus@northeastern.edu>
2026-08-12 07:45:24 -0400

    Time: 230 ms (95.71%) | Memory: 35.3 MB (72.85%) - LeetSync

--- 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.py
+++ 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.py
@@ -1,18 +1,22 @@
+
 class Solution:
     def maxSubarrayLength(self, nums: List[int], k: int) -> int:
-        
-
         mp = {}
+
         j = 0
         max_len = 0
+
         for i in range(len(nums)):
+            # Add current number into current window
+            mp[nums[i]] = mp.get(nums[i], 0) + 1
 
-            mp[nums[i]] = mp.get(nums[i],0)+1
+            # If current number appears more than k times,
+            # shrink window from the left
+            while mp[nums[i]] > k:
+                mp[nums[j]] -= 1
+                j += 1
 
-            while mp[nums[i]]>k:
-                mp[nums[j]]-=1
-                j+=1
-            
-            max_len = max(i-j+1,max_len)
+            # Update maximum valid window length
+            max_len = max(max_len, i - j + 1)
 
         return max_len
